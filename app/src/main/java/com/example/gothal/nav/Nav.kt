faeaf94171069226239bfef1bd81bfd3cdc8f090
Gray <mitchellbadri@gmail.com>
2025-12-23 00:54:23 -0500

    Added a home screen and routing

--- app/src/main/java/com/example/gothal/nav/Nav.kt
+++ app/src/main/java/com/example/gothal/nav/Nav.kt
@@ -1,23 +1,29 @@
 package com.example.gothal.nav
 
+import android.content.res.Resources
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.example.gothal.screens.HomeScreen
 
 class Routes{
     companion object{
@@ -39,7 +45,7 @@ fun Nav(){
                         horizontalArrangement = Arrangement.Center,
                         modifier = Modifier.fillMaxWidth()
                     ) {
-                        Text(text = "Gothal")
+                        Text(text = "The Gothal Companion", fontSize = 30.sp)
                     }
                 }
             )
@@ -66,7 +72,7 @@ fun Nav(){
             composable(
                 route = Routes.HOME
             ) {
-
+                HomeScreen()
             }
         }
     }
